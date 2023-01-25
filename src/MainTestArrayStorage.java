8c232ba32bf0e3edbc77bc70843ec05ea3ff56cd
Konstantin Mogilev <zzzummm272@gmail.com>
2023-01-25 23:07:52 +0400

    Add Resume constructor

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -5,12 +5,10 @@ public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
-        r1.uuid = "uuid1";
-        Resume r2 = new Resume();
-        r2.uuid = "uuid2";
-        Resume r3 = new Resume();
-        r3.uuid = "uuid3";
+        Resume r1 = new Resume("uuid1");
+        Resume r2 = new Resume("uuid2");
+        Resume r3 = new Resume("uuid3");
+
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
@@ -22,7 +20,7 @@ public class MainTestArrayStorage {
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(r1.uuid);
+        ARRAY_STORAGE.delete(r2.uuid);
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
--- src/Resume.java
+++ src/Resume.java
@@ -6,6 +6,12 @@ public class Resume {
     // Unique identifier
     String uuid;
 
+
+    public Resume(final String uuid) {
+        this.uuid = uuid;
+
+    }
+
     @Override
     public String toString() {
         return uuid;
