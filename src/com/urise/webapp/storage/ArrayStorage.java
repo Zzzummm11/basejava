961a8cd2accb9b4fd9e65c5871a8d293ad71ec21
Konstantin Mogilev <zzzummm272@gmail.com>
2023-01-31 21:27:41 +0400

    Introduce interface

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,7 +4,7 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public class ArrayStorage {
+public class ArrayStorage implements Storage {
     private static final int STORAGE_LIMIT = 10000;
     private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int count;
